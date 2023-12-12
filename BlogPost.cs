c7235828370318bed41a494f6cf36e27f98bdeaf
SillyHamster <34564789+RajkiranBargota@users.noreply.github.com>
2023-12-12 13:42:01 -0500

    Vaco test commit

---
+++ BlogPost.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace VacoTest
+{
+    public class BlogPost
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(75)]
+        public string Title { get; set; } = "";
+
+        [StringLength(5000)]
+        public string Contents { get; set; } = "";
+
+
+        public DateTime TimeStamp { get; set; }
+        public int Category { get; set; }
+
+    }
+}
---
+++ Category.cs
@@ -0,0 +1,8 @@
+﻿namespace VacoTest
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
